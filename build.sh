753d516062db53229a223b93644e8e9eac74a4cd
Josh Cox <josh@webhosting.coop>
2023-10-12 07:41:23 -0500

    dynamic group and list

--- build.sh
+++ build.sh
@@ -5,9 +5,8 @@ docker compose up -d
 sleep 5
 
 docker exec openldap ldapsearch -H ldap://localhost:1389 -LLL -x -s sub -b "dc=example,dc=net"
-docker exec openldap ldapsearch -H ldap://localhost:1389 -LLL -x -s sub -b "dc=example,dc=net" "cn=customuser" uid memberof
-
 exit 0
+docker exec openldap ldapsearch -H ldap://localhost:1389 -LLL -x -s sub -b "dc=example,dc=net" "cn=customuser" uid memberof
 docker exec openldap mkdir -p /tmp/slapd.d
 docker exec openldap mkdir -p /tmp/slapd-min.d
 docker exec openldap slaptest -f /slaptest/slapd.conf -F /tmp/slapd.d
